604ead520001ca52995a20b3d0e132acf87977b7
hallgato <hallgato@LABOR215-17>
2022-04-05 16:56:47 +0200

    gameItem created ball created pomg created

---
+++ LAB_09/Ball.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace LAB_09
+{
+    public class Ball : GameItem
+    {
+        private int centerX;
+        private int centerY;
+        private int radius;
+
+        private int speedX;
+        private int speedY;
+
+        public Ball(int centerX, int centerY, int radius, int speedX, int speedY)
+        {
+            this.centerX = centerX;
+            this.centerY = centerY;
+            this.radius = radius;
+            this.speedX = speedX;
+            this.speedY = speedY;
+        }
+
+        public void Move()
+        {
+            centerX += speedX;
+            centerY += speedY;
+        }
+
+        public void Collision()
+        {
+            speedX *= -1;
+            speedY *= -1;
+        }
+
+        public override Geometry Area
+        {
+            get
+            {
+                return new EllipseGeometry(new Point(centerX, centerY), radius, radius);
+            }
+        }
+
+    }
+}
---
+++ LAB_09/Pong.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace LAB_09
+{
+    public class Pong : GameItem
+    {       
+        private int centerX;
+        private int centerY;
+        private int size;
+        private int radiusX;
+        private int radiusY;
+
+        public Pong(int centerX, int centerY, int size, int radiusX, int radiusY)
+        {
+            this.centerX = centerX;
+            this.centerY = centerY;
+            this.size = size;
+            this.radiusX = radiusX;
+            this.radiusY = radiusY;
+        }
+
+        public override Geometry Area
+        {
+            get
+            {
+                Rect r = new Rect(30, 40, 100, 20);
+                return new RectangleGeometry(r, radiusX, radiusY);                           
+            }
+        }
+
+
+    }
+}
